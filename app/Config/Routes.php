1bf7883e9fb7ae61d5dfd81e517694c15d2bd752
Simon Weber <s.weber@mpr-net.com>
2023-01-11 14:39:14 +0100

    CI shield install

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,6 +40,8 @@ $routes->get('news', [News::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
 
+service('auth')->routes($routes);
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
